48b7016cd772c0233c08c8b7162e3adddf637d79
Leonardo Melo <leonardo.melo@rockspoon.com>
2023-08-04 16:25:54 -0300

    feat(awesome): update lain files

--- .config/awesome/lain/widget/bat.lua
+++ .config/awesome/lain/widget/bat.lua
@@ -1,8 +1,8 @@
 --[[
 
-    Licensed under GNU General Public License v2
-    * (c) 2013,      Luca CPZ
-    * (c) 2010-2012, Peter Hofmann
+	 Licensed under GNU General Public License v2
+	  * (c) 2013,      Luca CPZ
+	  * (c) 2010-2012, Peter Hofmann
 
 --]]
 
